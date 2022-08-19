788714652380ab02a7090c2605ce2a6f296bf863
marianacsm <mariana.martins@sptech.school>
2022-08-19 12:28:21 -0300

    atualizações banco e cálculo

---
+++ Banco de Dados/script-BD.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE REC;
+USE REC;
+DROP DATABASE REC;
+
+CREATE TABLE Empresa (
+    idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    razaoSocial VARCHAR(45) NOT NULL,
+    CNPJ CHAR(14) NOT NULL,
+    logradouro VARCHAR(45) NOT NULL,
+    numero INT NOT NULL,
+    bairro VARCHAR(45) NOT NULL,
+    cidade VARCHAR(45) NOT NULL,
+    estado CHAR(2) NOT NULL
+);
+
+CREATE TABLE Filial (
+    idFilial INT PRIMARY KEY AUTO_INCREMENT,
+    fkEmpresa INT,
+    FOREIGN KEY (fkEmpresa)
+        REFERENCES Empresa (idEmpresa),
+    nomeFilial VARCHAR(45) NOT NULL,
+    logradouroFilial VARCHAR(45) NOT NULL,
+    numeroFilial INT NOT NULL,
+    bairroFilial VARCHAR(45) NOT NULL,
+    cidadeFilial VARCHAR(45) NOT NULL,
+    estado VARCHAR(45) NOT NULL,
+    receita DECIMAL(8,1)
+)  AUTO_INCREMENT=10;
+
+CREATE TABLE Usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    fkFilial INT,
+    FOREIGN KEY (fkFilial)
+        REFERENCES Filial (idFilial),
+    nomeUsuario VARCHAR(45) NOT NULL,
+    emailUsuario VARCHAR(45) NOT NULL,
+    tipoLogin VARCHAR(45) NOT NULL
+)  AUTO_INCREMENT=100;
+
+CREATE TABLE Sala (
+    idSala INT PRIMARY KEY AUTO_INCREMENT,
+    fkFilial INT,
+    FOREIGN KEY (fkFilial)
+        REFERENCES Filial (idFilial),
+    numeroSala INT NOT NULL,
+    situacao VARCHAR(45) NOT NULL CHECK (situacao = 'disponivel'
+        OR situacao = 'indisponivel'
+        OR situacao = 'manutencao')
+)  AUTO_INCREMENT=500; 
+
+CREATE TABLE Totem (
+    idTotem INT PRIMARY KEY AUTO_INCREMENT,
+    fkFilial INT,
+    FOREIGN KEY (fkFilial)
+        REFERENCES Filial (idFilial),
+    modeloTotem VARCHAR(45) NOT NULL,
+    numeroSerial INT NOT NULL
+)  AUTO_INCREMENT=1000;
+
+CREATE TABLE Vendas (
+    idVenda INT PRIMARY KEY AUTO_INCREMENT,
+    fkTotem INT,
+    FOREIGN KEY (fkTotem)
+        REFERENCES Totem (idTotem),
+    tituloTransacao VARCHAR(45) NOT NULL,
+    valor DECIMAL(5 , 2 ) NOT NULL
+)  AUTO_INCREMENT=50;
+
+CREATE TABLE Leitura (
+    idLeitura INT PRIMARY KEY AUTO_INCREMENT,
+    fkTotem INT,
+    FOREIGN KEY (fkTotem)
+        REFERENCES Totem (idTotem),
+    cpuTotem INT NOT NULL,
+    memoriaTotem DECIMAL(2 , 1 ) NOT NULL,
+    discoTotem INT NOT NULL,
+    dataHora DATETIME NOT NULL
+);
+
+SELECT * FROM Empresa;
+SELECT * FROM Filial;
+SELECT * FROM Sala;
+SELECT * FROM Usuario;
+SELECT * FROM Totem;
+SELECT * FROM Leitura;
+SELECT * FROM Vendas;
+
+INSERT INTO empresa VALUES 
+(NULL, 'Cinemark Brasil S.A.', '00779721002357','Avenida Doutor Chucri Zaida',920,'Vila Cordeiro','São Paulo','SP');
+
+
--- Calculo Computacional/Python/scriptCapturaDados.py
+++ Calculo Computacional/Python/scriptCapturaDados.py
@@ -1,43 +1,66 @@
 
 import psutil
 import time
+from datetime import datetime
+
 
 def captura():
-    
+    print("Seja bem-vindo ao sistema de captura de dados do seu Hardware \U0001F604")
+    print("Menu de Opções: \n - CPU \n - Memoria \n - HD \n - Todos")
     desejo = bool(1)
-    componente = input("Qual componente do sistema operacional você deseja monitorar?")
-    tempo = int(input("Em quantos segundos você deseja acompanhar?"))
+    componente = input("\U0001F916 Qual componente do seu sistema operacional você deseja monitorar?")
+    tempo = int(input("\U0001F916 Em quantos segundos você deseja acompanhar?"))
     
     porcentagem =  psutil.cpu_percent(interval = None , percpu = False)
-    nucleos = psutil.cpu_count(logical=False)
-    processadoresLogicos = psutil.cpu_count()
-    frequencia =  psutil.cpu_freq(percpu=False)
-    cpu = psutil.cpu_times (percpu = False )
+    usoDisco = psutil.disk_usage('C:\\')
+    
+    # nucleos = psutil.cpu_count(logical=False)
+    # processadoresLogicos = psutil.cpu_count()
+    # frequencia =  psutil.cpu_freq(percpu=False)
+    # cpu = psutil.cpu_times (percpu = False )
 
     if (componente == "CPU" or componente == "cpu"):
-        print("Iniciando captura de dados da CPU...", "\n--------")
+        print("\U0001F750 Iniciando captura de dados da CPU...", "\n--------")
         while(desejo == 1):
-            print("Porcentagem de Utilização:{:.2f}".format(porcentagem),"\nNucleos:", nucleos, "\nProcessadores Logicos:{:.2f}".format(processadoresLogicos),
-                   "\nFrequencia da CPU:", frequencia, "\n--------", "\nUsuário", cpu[0], "\ntempo Sistema:{:.2f} Hrs".format(cpu[1]/3600), "\ntempo inativo:{:.2f}Hrs".format(cpu[2]/3600), "\nChamadas de procedimento adiadas:{:.2f}".format(cpu[3]))
+            dataHora = datetime.now()
+            dataHoraFormat = dataHora.strftime('%d/%m/%Y %H:%M:%S')
+
+            print("\U0001F4BB - Porcentagem de Utilização da CPU: {:.1f}%".format(porcentagem) ,"\n\U0001F55B - Data e Hora:", dataHoraFormat,"\n--------",)
+            # ,"\nNucleos:", nucleos, "\nProcessadores Logicos:{:.2f}".format(processadoresLogicos),
+            #        "\nFrequencia da CPU:", frequencia, "\n--------", "\nUsuário", cpu[0], "\ntempo Sistema:{:.2f} Hrs".format(cpu[1]/3600), "\ntempo inativo:{:.2f}Hrs".format(cpu[2]/3600), "\nChamadas de procedimento adiadas:{:.2f}".format(cpu[3])
             time.sleep(tempo)
             desejo = 1
-            
-    usoDisco = psutil.disk_usage('C:\\')
-    particoes = psutil. disk_partitions (all = False)
 
     if (componente == "HD" or componente == "hd" or componente == "Hd"):
-        print("Iniciando captura de dados do HD...", "\n--------")
+        print("\U0001F750 Iniciando captura de dados do Disco...", "\n--------")
         while(desejo == 1):
-            print("\nEspaço total:",usoDisco[0], "\nUsado:", usoDisco[1], "\nLivre:", usoDisco[2], "\nPorcentagem de uso:", usoDisco[3], "\n Partições:", particoes, "\n--------")
+            dataHora = datetime.now()
+            dataHoraFormat = dataHora.strftime('%d/%m/%Y %H:%M:%S')
+            print("\U0001F4BB - Porcentagem de Utilização do Disco:", usoDisco[3],'%', "\n\U0001F55B - Data e Hora:", dataHoraFormat,"\n--------")
+            # "\nEspaço total:",usoDisco[0], "\nUsado:", usoDisco[1], "\nLivre:", usoDisco[2],
             time.sleep(tempo)
             desejo = 1
             
-    if (componente == "memoria" or componente == "MEMORIA"):
-        print("Iniciando captura de dados da Memoria...", "\n--------")
+    if (componente == "memoria" or componente == "MEMORIA" or componente == "Memoria"):
+        print("Iniciando captura de dados da Memoria...\U0001F4AD", "\n--------")
         while(desejo == 1):
-            print ("Memoria virtual:",psutil.virtual_memory(), "\n--------")
+            dataHora = datetime.now()
+            dataHoraFormat = dataHora.strftime('%d/%m/%Y %H:%M:%S')
+            print ("\U0001F4BB - Porcentagem de Utilização da Memoria:",psutil.virtual_memory().percent, '%',"\n\U0001F55B - Data e Hora:", dataHoraFormat, "\n--------")
             time.sleep(tempo)
             desejo = 1
+
+    if (componente == "TODOS" or componente == "todos" or componente == "Todos"):
+        print("\U0001F750 Iniciando captura dos dados...", "\n--------")
+        while(desejo == 1):
+            dataHora = datetime.now()
+            dataHoraFormat = dataHora.strftime('%d/%m/%Y %H:%M:%S')
+            print ("\U0001F4BB - Porcentagem de Utilização da CPU: {:.1f}%".format(porcentagem),
+            "\n\U0001F4BB - Porcentagem de Utilização do Disco:", usoDisco[3],'%',
+            "\n\U0001F4BB - Porcentagem de Utilização da Memoria:",psutil.virtual_memory().percent,'%',"\n\U0001F55B - Data e Hora:", dataHoraFormat,"\n--------")
+            time.sleep(tempo)
+            desejo = 1        
+
 
 captura()
 
