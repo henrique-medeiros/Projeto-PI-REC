aa26896989ecd8acfc88438b51cfdcd3add5af3d
GOGoncalves <gustavo.goncalves@sptech.school>
2022-09-05 14:09:28 -0300

    Atualizacao Modelagem/Script/Python

---
+++ API PYTHON/script-BD.sql
@@ -0,0 +1,152 @@
+CREATE DATABASE REC;
+USE REC;
+-- DROP DATABASE REC;
+
+CREATE TABLE Empresa (
+    idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    razaoSocial VARCHAR(45) NOT NULL,
+    CNPJ CHAR(14) NOT NULL,
+    logradouro VARCHAR(45) NOT NULL,
+    numero INT NOT NULL,
+    bairro VARCHAR(45) NOT NULL,
+    cidade VARCHAR(45) NOT NULL,
+    estado CHAR(2) NOT NULL
+);
+
+CREATE TABLE Licenca (
+idLicenca INT PRIMARY KEY,
+fkEmpresa INT,
+FOREIGN KEY (fkEmpresa)
+	REFERENCES Empresa (idEmpresa),
+tipoLicenca VARCHAR(45) NOT NULL CHECK (tipoLicenca = 'analista'
+        OR tipoLicenca = 'gerenciaFilial'
+        OR tipoLicenca = 'gerenciaMatriz')
+);
+
+CREATE TABLE Filial (
+    idFilial INT PRIMARY KEY AUTO_INCREMENT,
+    fkEmpresa INT,
+    FOREIGN KEY (fkEmpresa)
+        REFERENCES Empresa (idEmpresa),
+    nomeFantasia VARCHAR(45) NOT NULL,
+    logradouro VARCHAR(45) NOT NULL,
+    numero INT NOT NULL,
+    bairro VARCHAR(45) NOT NULL,
+    cidade VARCHAR(45) NOT NULL,
+    estado CHAR(2) NOT NULL,
+    receita DECIMAL(8,1)
+)  AUTO_INCREMENT=10;
+
+CREATE TABLE Usuario (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    fkFilial INT,
+    FOREIGN KEY (fkFilial)
+        REFERENCES Filial (idFilial),
+	fkLicenca INT,
+    FOREIGN KEY (fklicenca)
+		REFERENCES Licenca (idLicenca),
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    senha VARCHAR(45) NOT NULL
+)  AUTO_INCREMENT=100;
+
+CREATE TABLE Sala (
+    idSala INT PRIMARY KEY AUTO_INCREMENT,
+    fkFilial INT,
+    FOREIGN KEY (fkFilial)
+        REFERENCES Filial (idFilial),
+    numeroSala INT NOT NULL,
+    situacao VARCHAR(45) NOT NULL CHECK (situacao = 'disponivel'
+        OR situacao = 'indisponivel'
+        OR situacao = 'manutencao')
+)  AUTO_INCREMENT=500; 
+
+CREATE TABLE Atm (
+    idAtm INT PRIMARY KEY AUTO_INCREMENT,
+    fkFilial INT,
+    FOREIGN KEY (fkFilial)
+        REFERENCES Filial (idFilial),
+	nome VARCHAR(45) NOT NULL,
+    maquina VARCHAR(45) NOT NULL,
+    sistemaOp VARCHAR(45) NOT NULL
+)  AUTO_INCREMENT=1000;
+
+CREATE TABLE Vendas (
+    idVenda INT PRIMARY KEY AUTO_INCREMENT,
+    fkFilial INT,
+    FOREIGN KEY (fkFilial)
+        REFERENCES Filial (idFilial),
+    transacao VARCHAR(45) NOT NULL CHECK (transacao = 'Bomboniere'
+        OR transacao = 'Ingressos'),
+    qtdTransacao INT NOT NULL,
+    valor FLOAT NOT NULL
+)  AUTO_INCREMENT=50;
+
+-- NÃO CRIAR A TABELA LEITURA, A API PYTHON VAI CRIAR
+CREATE TABLE Leitura (
+    idLeitura INT PRIMARY KEY AUTO_INCREMENT,
+    fkAtm INT,
+    FOREIGN KEY (fkAtm)
+        REFERENCES Atm (idAtm),
+    cpuTotem FLOAT NOT NULL,
+    memoriaTotem FLOAT NOT NULL,
+    discoTotem FLOAT NOT NULL,
+    dataHora DATETIME NOT NULL
+);
+
+-- Inserção e visualização de dados em tabelas
+
+-- Inserção feita pela REC
+DESC Empresa;
+INSERT INTO Empresa 
+	VALUES (NULL, 'Cinemark Brasil S.A.', '00779721002357','Avenida Doutor Chucri Zaida',920,'Vila Cordeiro','São Paulo','SP');
+SELECT * FROM Empresa;
+
+-- Inserção feita pelo Usuario
+DESC Filial;
+INSERT INTO Filial 
+	VALUES (NULL,1, 'Cinemark Tatuape', 'Endereço Teste', 100, 'Vila Lageado', 'São Paulo', 'SP',8000000.0);
+SELECT * FROM Filial;
+
+-- Inserção feita pelo Usuario
+DESC Sala;
+INSERT INTO Sala 
+	VALUES (NULL, 10, 1,"disponivel");
+SELECT * FROM Sala;
+
+-- Inserção feita pela REC
+DESC Licenca;
+INSERT INTO Licenca
+	VALUES (105345,1, "gerenciaMatriz"),
+			(105346,1, "gerenciaFilial"),
+            (105347,1, "analista");
+SELECT * FROM Licenca;
+
+-- Inserção feita pelo Usuario
+DESC Usuario;
+INSERT INTO Usuario
+	VALUES (NULL, 10,105345, "fernandoBrandao", "fernando.brandao@sptech.school", "12345");
+SELECT * FROM Usuario;
+
+-- Inserção feita pela API REC (NÃO EXECUTAR O INSERT)
+DESC Atm;
+-- INSERT INTO Atm
+-- VALUES ();
+-- INSERT INTO Atm (fkFilial, nome, maquina, sistemaOp) VALUES (10, "nomeMaquina" , "maquina", "sistema");
+SELECT * FROM Atm;
+TRUNCATE Atm;
+
+-- Inserção feita pela API REC (NÃO EXECUTAR O INSERT)
+DESC Leitura;
+-- INSERT INTO Leitura
+-- VALUES ();
+-- INSERT INTO Leitura (fkAtm, cpuTotem, memoriaTotem, discoTotem, dataHora) VALUES (1000, 3232 , 323, 32131,'2022-10-21 10:06:23');
+SELECT * FROM Leitura;
+DROP TABLE Leitura;
+TRUNCATE Leitura;
+
+-- Inserção feita pela REC
+DESC Vendas;
+INSERT INTO Vendas
+	VALUES (NULL, 10, "bomboniere",5,500);
+SELECT * FROM Vendas;
---
+++ API PYTHON/scriptCapturaDados.py
@@ -0,0 +1,150 @@
+# GRUPO 1 - PI - REC
+# Eduardo Bengattini RA: 03221008
+# Erika Mendes RA: 03221023
+# Gabriel Antonio RA: 03221012
+# Gustavo Golçalves RA: 03221046
+# Mariana Cabral RA: 03221061
+# Patrick Herold RA: 03221054
+
+# Importações de Bibliotecas:
+import psutil
+import time
+from datetime import datetime
+import platform
+import mysql.connector
+from mysql.connector import errorcode
+
+# Função captura de dados de maquina:
+def captura(conn):
+
+    cursor = conn.cursor()
+    print("Seja bem-vindo ao sistema de captura de dados do seu Hardware \U0001F604")
+    print("Menu de Opções: \n - CPU \n - Memoria \n - HD \n - Todos")
+
+    componente = input(
+        "\U0001F916 Qual componente do seu sistema operacional você deseja monitorar?")
+    tempo = int(input("\U0001F916 Em quantos segundos você deseja acompanhar?"))
+
+    meu_sistema = platform.uname()
+    porcentagem = psutil.cpu_percent(interval=None, percpu=False)
+    usoDisco = psutil.disk_usage('C:\\')[3]
+    sistema = meu_sistema.system
+    maquina = meu_sistema.machine
+    nomeMaquina = meu_sistema.node
+    memoria = psutil.virtual_memory().percent
+
+    desejo = bool(1)
+    if (componente == "CPU" or componente == "cpu"):
+        print("\U0001F750 Iniciando captura de dados da CPU...", "\n--------")
+        while(desejo == 1):
+            dataHora = datetime.now()
+            dataHoraFormat = dataHora.strftime('%d/%m/%Y %H:%M:%S')
+
+            print("\U0001F4BB - Porcentagem de Utilização da CPU: {:.1f}%".format(
+                porcentagem), "\n\U0001F55B - Data e Hora:", dataHoraFormat, "\n--------",)
+
+            time.sleep(tempo)
+            desejo = 1
+
+    if (componente == "HD" or componente == "hd" or componente == "Hd"):
+        print("\U0001F750 Iniciando captura de dados do Disco...", "\n--------")
+        while(desejo == 1):
+            dataHora = datetime.now()
+            dataHoraFormat = dataHora.strftime('%d/%m/%Y %H:%M:%S')
+            print("\U0001F4BB - Porcentagem de Utilização do Disco:", usoDisco,
+                  '%', "\n\U0001F55B - Data e Hora:", dataHoraFormat, "\n--------")
+            # "\nEspaço total:",usoDisco[0], "\nUsado:", usoDisco[1], "\nLivre:", usoDisco[2],
+            time.sleep(tempo)
+            desejo = 1
+
+    if (componente == "memoria" or componente == "MEMORIA" or componente == "Memoria"):
+        print("Iniciando captura de dados da Memoria...\U0001F4AD", "\n--------")
+        while(desejo == 1):
+            dataHora = datetime.now()
+            dataHoraFormat = dataHora.strftime('%d/%m/%Y %H:%M:%S')
+            print("\U0001F4BB - Porcentagem de Utilização da Memoria:", psutil.virtual_memory(
+            ).percent, '%', "\n\U0001F55B - Data e Hora:", dataHoraFormat, "\n--------")
+            time.sleep(tempo)
+            desejo = 1
+
+    if (componente == "TODOS" or componente == "todos" or componente == "Todos"):
+        print("\U0001F750 Iniciando captura dos dados...", "\n--------")
+        # Executar o drop se a tabela Leitura existir no banco!!
+        # cursor.execute("DROP TABLE Leitura;")
+        cursor.execute("TRUNCATE Atm;")
+        # ATM 1 (Maquina 1)
+        if (sistema == "Windows"):
+            cursor.execute("INSERT INTO Atm (fkFilial, nome, maquina, sistemaOp) VALUES (%s, %s, %s,%s);",
+                           (10, nomeMaquina, maquina, sistema))
+        elif (sistema == "Linux"):
+            cursor.execute("INSERT INTO Atm (fkFilial, nome, maquina, sistemaOp) VALUES (%s, %s, %s,%s);",
+                           (10, nomeMaquina, maquina, sistema))
+        # ATM 2 (Maquina 2)
+        if (sistema == "Windows"):
+            cursor.execute("INSERT INTO Atm (fkFilial, nome, maquina, sistemaOp) VALUES (%s, %s, %s,%s);",
+                           (10, "Maquina 2", "AMD32", "Linux"))
+        elif (sistema == "Linux"):
+            cursor.execute("INSERT INTO Atm (fkFilial, nome, maquina, sistemaOp) VALUES (%s, %s, %s,%s);",
+                           (10, "Maquina 2", "AMD32", "Windows"))
+        # ATM 3 (Maquina 3)
+        if (sistema == "Windows"):
+            cursor.execute("INSERT INTO Atm (fkFilial, nome, maquina, sistemaOp) VALUES (%s, %s, %s,%s);",
+                           (10, "Maquina 3", maquina, "MacOS"))
+        elif (sistema == "Linux"):
+            cursor.execute("INSERT INTO Atm (fkFilial, nome, maquina, sistemaOp) VALUES (%s, %s, %s,%s);",
+                           (10, "Maquina 3", maquina, "Linux"))
+
+        cursor.execute("CREATE TABLE Leitura (idLeitura INT PRIMARY KEY AUTO_INCREMENT, fkAtm INT, FOREIGN KEY (fkAtm)REFERENCES Atm (idAtm), cpuTotem FLOAT NOT NULL, memoriaTotem FLOAT NOT NULL, discoTotem FLOAT NOT NULL, dataHora DATETIME NOT NULL);")
+
+        while(desejo == 1):
+            dataHora = datetime.now()
+            dataHoraFormat = dataHora.strftime('%Y/%m/%d %H:%M:%S')
+
+            print("\U0001F4BB - Porcentagem de Utilização da CPU: {:.1f}%".format(porcentagem),
+                  "\n\U0001F4BB - Porcentagem de Utilização do Disco:", usoDisco, '%',
+                  "\n\U0001F4BB - Porcentagem de Utilização da Memoria:", psutil.virtual_memory().percent, '%', "\n\U0001F55B - Data e Hora:", dataHoraFormat, "\n--------")
+
+            # Simulação de ATMs:
+
+            CPU3 = porcentagem * 1.15
+            CPU2 = CPU3 - 0.05
+
+            M3 = memoria * 1.10
+            M2 = memoria * 1.15
+
+            D2 = usoDisco - 0.05
+            D3 = D2 * 3
+
+            cursor.execute("INSERT INTO Leitura (fkAtm, cpuTotem, memoriaTotem, discoTotem, dataHora) VALUES (%s,%s, %s, %s,%s);",
+                           (1, porcentagem, memoria, usoDisco, dataHoraFormat))
+            cursor.execute("INSERT INTO Leitura (fkAtm, cpuTotem, memoriaTotem, discoTotem, dataHora) VALUES (%s,%s, %s, %s,%s);",
+                           (2, CPU2, M2, D2, dataHoraFormat))
+            cursor.execute("INSERT INTO Leitura (fkAtm, cpuTotem, memoriaTotem, discoTotem, dataHora) VALUES (%s,%s, %s, %s,%s);",
+                           (3, CPU3, M3, D3, dataHoraFormat))
+
+            conn.commit()
+            time.sleep(tempo)
+            desejo = 1
+
+
+try:
+    # Conexão com o banco de dados
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='COLOQUE SUA SENHA ROOT AQUI',
+        database='REC'
+    )
+    print("Conexão com o Banco de Dados MySQL efetuada com sucesso.")
+    captura(conn)
+
+    # Validações de Erro:
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Algo está errado com o username ou a senha")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Banco de dados não existe")
+    else:
+        print(err)
+else:
+    cursor = conn.cursor()
