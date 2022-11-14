e9daeb87ba9b0e55f5b4ea86445bfd321f72a1bd
MauricioUesso <mauricio.martins@sptech.school>
2022-11-14 12:08:34 -0300

    Dash funcional e dinâmica

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -4,57 +4,76 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
 
     instrucaoSql = ''
 
-
-
-    const hoje = new Date();
-    const horaInicial = 8;
-    const data = `${hoje.getFullYear()}-${hoje.getMonth()+1}-${hoje.getDate()}`;
-
-    instrucaoSql = 'select * from (';
-    instrucaoSql += `
-    (select SUM(chave) as contagem, '${horaInicial.toString().padStart(2, '0')}:00' as 'momento_grafico' from medida 
-        where momento >= '${data} ${horaInicial}:00:00' and momento < '${data} ${horaInicial+1}:00:00')
-    `;
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top ${limite_linhas} cpuTotem as cpu, memoriaTotem as memoria, discoTotem as disco, dataHora,
+        CONVERT(varchar, dataHora, 108) as momento_grafico from Leitura where fkAtm = ${idAquario}
+        order by idLeitura desc`;
     
-    
-    const horaFinal = new Date().getHours() + 1;
-    for (let hora = horaInicial+1; hora < horaFinal; hora++) {
-        instrucaoSql += ` UNION 
-        (select SUM(chave) as contagem, '${hora.toString().padStart(2, '0')}:00' as 'momento_grafico' from medida 
-        where momento >= '${data} ${hora}:00:00' and momento < '${data} ${hora+1}:00:00')
-       `
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select cpuTotem as cpu, memoriaTotem as memoria, discoTotem as disco, 
+                        dataHora,
+                        DATE_FORMAT(dataHora,'%H:%i:%s') as momento_grafico
+                    from leitura
+                    where fkAtm = ${idAquario}
+                    order by idLeitura desc ${limite_linhas}`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
     }
 
-    instrucaoSql += ') as u ORDER by u.momento_grafico;';
-        
+
+
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
+
+
+
+    // const hoje = new Date();
+    // const horaInicial = 8;
+    // const data = `${hoje.getFullYear()}-${hoje.getMonth()+1}-${hoje.getDate()}`;
+
+    // instrucaoSql = 'select * from (';
+    // instrucaoSql += `
+    // (select SUM(chave) as contagem, '${horaInicial.toString().padStart(2, '0')}:00' as 'momento_grafico' from medida 
+    //     where momento >= '${data} ${horaInicial}:00:00' and momento < '${data} ${horaInicial+1}:00:00')
+    // `;
+
+
+    // const horaFinal = new Date().getHours() + 1;
+    // for (let hora = horaInicial+1; hora < horaFinal; hora++) {
+    //     instrucaoSql += ` UNION 
+    //     (select SUM(chave) as contagem, '${hora.toString().padStart(2, '0')}:00' as 'momento_grafico' from medida 
+    //     where momento >= '${data} ${hora}:00:00' and momento < '${data} ${hora+1}:00:00')
+    //    `
+    // }
+
+    // instrucaoSql += ') as u ORDER by u.momento_grafico;';
+
+    // console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    // return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idAquario) {
 
     instrucaoSql = ''
 
-/*     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fkAquario 
-                        from medida where fkAquario = ${idAquario} 
-                    order by id desc`;
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select cpuTotem as cpu, memoriaTotem as memoria, discoTotem as disco, dataHora,
+        CONVERT(varchar, dataHora, 108) as momento_grafico from Leitura join Atm on fkAtm = idAtm 
+        order by idLeitura desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select sum(chave), DATE_FORMAT(momento, '%H:%i:%s') as momento_grafico, fk_Aquario
-		from medida 
-				join aquario
-					on fk_Aquario = ${idAquario}
-		order by idMedida`;
+        instrucaoSql = `select cpuTotem as cpu, memoriaTotem as memoria, discoTotem as disco, 
+        dataHora,
+        DATE_FORMAT(dataHora,'%H:%i:%s') as momento_grafico
+        from leitura
+        where fkAtm = ${idAquario}
+         order by idLeitura desc ${limite_linhas}`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
     }
- */
+
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
