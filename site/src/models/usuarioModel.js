02c4edf5020963b96facd44eb3badd06b60e94a9
Larissagouveia <larissa.gouveia@sptech.school>
2022-11-23 14:33:50 -0300

    alertas integrado com pipefy

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -12,7 +12,7 @@ function listar() {
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM usuario WHERE emailUsuario = '${email}' AND senhaUsuario = '${senha}';
+        SELECT  usuario.*, Filial.nomeFantasia FROM usuario join Filial on fkFilial = idFilial WHERE emailUsuario = '${email}' AND senhaUsuario = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
